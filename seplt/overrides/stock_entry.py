4b450279628b92963ae845b800108b25cd0fb38e
Sanket Shah <shahsanket322003@gmail.com>
2026-08-07 12:32:57 +0000

    fix: by pass more validation for subcontracting inward

--- seplt/overrides/stock_entry.py
+++ seplt/overrides/stock_entry.py
@@ -1,12 +1,19 @@
+from erpnext.buying.utils import check_on_hold_or_closed_status
 from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 
 
 class CustomStockEntry(StockEntry):
-	def create_stock_reservation_entries_for_inward(self):
-		# Skip creating Stock Reservation Entries when a "Receive from
-		# Customer" Stock Entry is submitted.
-		pass
+    def create_stock_reservation_entries_for_inward(self):
+        return
 
-	def cancel_stock_reservation_entries_for_inward(self):
-		# No reservation entries are created on submit, so nothing to cancel.
-		pass
+    def cancel_stock_reservation_entries_for_inward(self):
+        return
+
+    def validate_subcontracting_inward(self):
+        return
+
+    def on_submit_subcontracting_inward(self):
+        return
+
+    def on_cancel_subcontracting_inward(self):
+        return
